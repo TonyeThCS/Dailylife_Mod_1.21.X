42e8be17e94ca3b67c1e57e214fc3d50706a4004
Tonye <tonye.thom@gmail.com>
2025-08-15 21:35:29 +0800

    Initial commit

---
+++ src/main/java/com/tonye/dailylife/DailyLifeClient.java
@@ -0,0 +1,10 @@
+package com.tonye.dailylife;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DailyLifeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
