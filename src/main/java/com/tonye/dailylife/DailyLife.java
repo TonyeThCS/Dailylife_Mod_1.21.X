a4ecfcffb6108e83d9a9417312eb2a3d7a1353a1
Tonye <tonye.thom@gmail.com>
2025-08-15 21:40:22 +0800

    Testing Phase Time!

--- src/main/java/com/tonye/dailylife/DailyLife.java
+++ src/main/java/com/tonye/dailylife/DailyLife.java
@@ -5,6 +5,8 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+//Testing Phase
+
 public class DailyLife implements ModInitializer {
 	public static final String MOD_ID = "dailylife";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
